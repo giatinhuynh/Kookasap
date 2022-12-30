62c72a85f00fcb4a8419c02b65cafb1fb8a20b52
Restyled.io <commits@restyled.io>
2022-12-30 20:47:40 +0000

    Restyled by prettier

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const searchBtn = document.getElementById('search-btn');
-const mealList = document.getElementById('meal');
-const mealDetailsContent = document.querySelector('.meal-details-content');
-const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const searchBtn = document.getElementById("search-btn");
+const mealList = document.getElementById("meal");
+const mealDetailsContent = document.querySelector(".meal-details-content");
+const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
 // event listeners
-searchBtn.addEventListener('click', getMealList);
-mealList.addEventListener('click', getMealRecipe);
-recipeCloseBtn.addEventListener(
-    'click',
-    () => { mealDetailsContent.parentElement.classList.remove('showRecipe'); });
+searchBtn.addEventListener("click", getMealList);
+mealList.addEventListener("click", getMealRecipe);
+recipeCloseBtn.addEventListener("click", () => {
+  mealDetailsContent.parentElement.classList.remove("showRecipe");
+});
 
 // get meal list that matches with the ingredients
 function getMealList() {
-  let searchInputTxt = document.getElementById('search-input').value.trim();
+  let searchInputTxt = document.getElementById("search-input").value.trim();
   // split the ingredients by comma and join them with a comma
-  let ingredients = searchInputTxt.split(',').join(',');
+  let ingredients = searchInputTxt.split(",").join(",");
   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`)
-      .then(response => response.json())
-      .then(data => {
-        let html = "";
-        if (data.meals) {
-          data.meals.forEach(meal => {
-            html += `
+    .then((response) => response.json())
+    .then((data) => {
+      let html = "";
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          html += `
                     <div class = "meal-item" data-id = "${meal.idMeal}">
                         <div class = "meal-img">
                             <img src = "${meal.strMealThumb}" alt = "food">
@@ -32,26 +32,27 @@ function getMealList() {
                         </div>
                     </div>
                 `;
-          });
-          mealList.classList.remove('notFound');
-        } else {
-          html = "Sorry, we didn't find any meal!";
-          mealList.classList.add('notFound');
-        }
+        });
+        mealList.classList.remove("notFound");
+      } else {
+        html = "Sorry, we didn't find any meal!";
+        mealList.classList.add("notFound");
+      }
 
-        mealList.innerHTML = html;
-      });
+      mealList.innerHTML = html;
+    });
 }
 
 // get recipe of the meal
 function getMealRecipe(e) {
   e.preventDefault();
-  if (e.target.classList.contains('recipe-btn')) {
+  if (e.target.classList.contains("recipe-btn")) {
     let mealItem = e.target.parentElement.parentElement;
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${
-              mealItem.dataset.id}`)
-        .then(response => response.json())
-        .then(data => mealRecipeModal(data.meals));
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => mealRecipeModal(data.meals));
   }
 }
 
@@ -74,5 +75,5 @@ function mealRecipeModal(meal) {
         </div>
     `;
   mealDetailsContent.innerHTML = html;
-  mealDetailsContent.parentElement.classList.add('showRecipe');
+  mealDetailsContent.parentElement.classList.add("showRecipe");
 }
